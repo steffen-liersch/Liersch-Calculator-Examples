ab8ad26235abafdf10de37fadc3ea21a419d5760
steffen-liersch <S.Liersch@gmx.de>
2023-12-02 19:03:40 +0100

    Change the target framework to .NET 8.0 and simplify the output path

--- CSharp/Calculator.Tests/Helpers.cs
+++ CSharp/Calculator.Tests/Helpers.cs
@@ -16,7 +16,7 @@ static class Helpers
 {
   public static string LoadTests()
   {
-    string fn = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../../../Unit-Testing/tests.json");
+    string fn = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../../Unit-Testing/tests.json");
     return File.ReadAllText(fn, Encoding.UTF8);
   }
 
